aad9b03697a8d443bc94b34949156b8d8a5f479d
ManuelTest <manueltesting01@gmail.com>
2025-09-15 10:13:09 +0200

    Update Win11Debloat.ps1

--- Win11Debloat.ps1
+++ Win11Debloat.ps1
@@ -3,6 +3,9 @@
 [CmdletBinding()]
 param (
 	[switch]$RestorePoint,
+ 	[switch]$PowerSettings,
+  	[switch]$ActivateUltimatePlan,
+  	[switch]$DisablePowerSaving,
     [switch]$ActivateWindows,
     [switch]$PauseUpdates,
     [switch]$Runtimes,
@@ -69,6 +72,79 @@ function Create-RestorePoint {
     if (-not (Get-ComputerRestorePoint | Where-Object { $_.CreationTime.Date -eq (Get-Date).Date })) {
         Checkpoint-Computer -Description "Restore Point" -RestorePointType "MODIFY_SETTINGS" | Out-Null
     }
+}
+
+function Activate-UltimatePlan {
+	# Activate Ultimate Performance Power Plan
+	Write-Output "Activating Ultimate Performance Power Plan..."
+	# Import the Ultimate Performance power plan and create a custom duplicate with a fixed GUID
+	cmd /c "powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 99999999-9999-9999-9999-999999999999 >nul 2>&1"
+	# Set the custom Ultimate Performance power plan as the active scheme
+	cmd /c "powercfg /SETACTIVE 99999999-9999-9999-9999-999999999999 >nul 2>&1"
+	# Disable Windows Fast Startup to ensure clean system initialization on boot
+	cmd /c "reg add `"HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power`" /v `"HiberbootEnabled`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+	# Force all CPU cores to remain unparked for maximum responsiveness
+	cmd /c "reg add `"HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583`" /v `"ValueMax`" /t REG_DWORD /d `"0`" /f >nul 2>&1"	
+	# Prevent hard disks from turning off while on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0x00000000 
+	# Pause desktop background slideshow to avoid unnecessary resource usage
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 0d7dbae2-4294-402a-ba8e-26777e8488cd 309dce9b-bef4-4119-9921-a851fb12f0f4 001 
+	# Set wireless adapter to maximum performance on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 000
+	# Disable automatic sleep on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 238c9fa8-0aad-41ed-83f4-97be242c8f20 29f6c1db-86da-48c5-9fdb-f2b67b1f44da 0x00000000 
+	# Disable hybrid sleep on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 000 
+	# Disable hibernation timeout on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 238c9fa8-0aad-41ed-83f4-97be242c8f20 9d7815a6-7ee4-497e-8888-515a05f02364 0x00000000 
+	# Prevent scheduled tasks from waking the computer while on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d 000 
+	# Configure power button to perform a full shutdown when pressed
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 4f971e89-eebd-4455-a8de-9e59040e7347 a7066653-8d6c-40a8-910e-a1f54b84c7e5 002 
+	# Disable PCI Express Link State Power Management for maximum throughput
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 000 
+	# Set minimum processor state to 100% for consistent maximum performance
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 0x00000064 
+	# Enable active cooling policy (fan priority over throttling)
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 54533251-82be-4824-96c1-47b60b740d00 94d3a615-a899-4ac5-ae2b-e4d8f634367f 001 
+	# Set maximum processor state to 100% on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 0x00000064 
+	# Set display brightness to 100% when on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 7516b95f-f776-4464-8c53-06167f40cc99 aded5e82-b909-4619-9949-f5d71dac0bcb 0x00000064
+	# Set dimmed display brightness to 100% on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 7516b95f-f776-4464-8c53-06167f40cc99 f1fbfde2-a960-4165-9f88-50667911ce96 0x00000064 
+	# Disable adaptive brightness on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 7516b95f-f776-4464-8c53-06167f40cc99 fbd9aa66-9553-4097-ba44-ed6e9d65eab8 000 
+	# Prioritize performance over quality in video playback when on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 9596fb26-9850-41fd-ac3e-f7c3c00afd4b 10778347-1370-4ee0-8bbd-33bdacaade49 001 
+	# Optimize video playback for performance on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 9596fb26-9850-41fd-ac3e-f7c3c00afd4b 34c7b99f-9a6d-4b3c-8dc7-b6693b78cef4 000 
+	# Force Intel integrated graphics to maximum performance on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 44f3beca-a7c0-460e-9df2-bb8b99e0cba6 3619c3f2-afb2-4afc-b0e9-e7fef372de36 002 *>$null
+	# Set AMD Power Slider to best performance on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 c763b4ec-0e50-4b6b-9bed-2b92a6ee884e 7ec1751b-60ed-4588-afb5-9819d3d77d90 003 *>$null 
+	# Force AMD/ATI PowerPlay graphics mode to maximize performance
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 f693fb01-e858-4f00-b20f-f30e12ac06d6 191f65b5-d45c-4a4f-8aae-1ab8bfd980e6 001 *>$null
+	# Force dynamic graphics switching to maximum performance mode
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e276e160-7cb0-43c6-b20b-73f5dce39954 a1662ab2-9d34-4e53-ba8b-2639b9e20857 003 *>$null 
+	# Disable critical battery notification on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f 5dbb7c9f-38e9-40d2-9749-4f8a0e9f640f 000
+	# Set critical battery action to "do nothing" when on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f 637ea02f-bbcb-4015-8e2c-a1c7b9c0b546 000
+	# Set low battery threshold to 0% on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f 8183ba9a-e910-48da-8769-14ae6dc1170a 0x00000000 
+	# Set critical battery threshold to 0% on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f 9a66d8d7-4ff7-4ef9-b5a2-5a326ca2a469 0x00000000 
+	# Disable low battery notification on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f bcded951-187b-4d05-bccc-f7e51960c258 000 
+	# Set low battery action to "do nothing" on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f d8742dcb-3e6a-4b3c-b3fe-374623cdcf06 000 
+	# Set reserve battery threshold to 0% on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 e73a048d-bf27-4f12-9731-8b2076e8891f f3c5027d-cd16-4930-aa6b-90db844a8f00 0x00000000 
+	# Ensure brightness is not reduced by battery saver mode on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 de830923-a562-41af-a086-e3a2c6bad2da 13d09884-f74e-474a-a852-b6bde8ad03a8 0x00000064 
+	# Prevent battery saver from activating automatically on AC power
+	powercfg /setacvalueindex 99999999-9999-9999-9999-999999999999 de830923-a562-41af-a086-e3a2c6bad2da e69653ca-cf7f-4f05-aa73-cb833fa90ad4 0x00000000
 }
 
 function Activate-Windows {
@@ -10362,6 +10438,22 @@ if ($RestorePoint) {
 	Create-RestorePoint
 }
 
+# POWER SETTINGS
+if ($PowerSettings) {
+	Activate-UltimatePlan
+ 	Disable-PowerSaving
+}
+
+# ACTIVATE ULTIMATE POWER PLAN
+if ($ActivateUltimatePlan) {
+	Activate-UltimatePlan
+}
+
+# DISABLE POWER SAVING
+if ($DisablePowerSaving) {
+	Disable-PowerSaving
+}
+
 # ACTIVATE WINDOWS
 if ($ActivateWindows) {
 	Activate-Windows
@@ -10509,6 +10601,7 @@ Write-Output ""
 
 Write-Output "Script execution completed."
 pause
+
 
 
 
